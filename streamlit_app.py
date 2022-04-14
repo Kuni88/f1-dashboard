7231c21d0a53b99cab1bf9270c78f69a0e834719
Kunihiro <kunihiro.takeoka.93@gmail.com>
2022-04-14 23:30:43 +0900

    black

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,19 @@
 import fastf1
 import streamlit as st
-import os 
+import os
 from fastf1 import plotting
 from matplotlib import pyplot as plt
 
 
+st.set_page_config(page_title="F1 Dashboard", page_icon="🏎")
+
+st.title("🏁GP Dashboard🏎")
+
 plotting.setup_mpl()
 
-if not os.path.isdir('.cache'):
-    os.mkdir('.cache')
-fastf1.Cache.enable_cache('.cache')
+if not os.path.isdir(".cache"):
+    os.mkdir(".cache")
+fastf1.Cache.enable_cache(".cache")
 
 
 YEARS = [2019, 2020, 2021, 2022]
@@ -23,14 +27,14 @@ for year in YEARS:
             name = fastf1.get_event(year, index).EventName
             lists_of_gp_years[year].append(name)
             index += 1
-        except Exception as e :
+        except Exception as e:
             print(e, year, index)
             break
 
 year = st.selectbox("Year", YEARS)
 gp_index_or_name = st.selectbox("Grand Prix", lists_of_gp_years[year])
 
-race = fastf1.get_session(year, gp_index_or_name, 'Race')
+race = fastf1.get_session(year, gp_index_or_name, "Race")
 race.load()
 
 drivers = [race.get_driver(num) for num in race.drivers]
@@ -39,24 +43,26 @@ for driver in drivers:
     drivers_info[driver.Abbreviation] = (driver.TeamName, driver.TeamColor)
 
 
-selected_racers = st.multiselect(
-     'Drivers',
-     list(drivers_info.keys()),
-     ['LEC', 'HAM']
-)
+selected_racers = st.multiselect("Drivers", list(drivers_info.keys()), ["LEC", "HAM"])
 
 ### Plot
-plot_type = '.'
+plot_type = "."
 fig, ax = plt.subplots()
 teams = []
 for racer_name in selected_racers:
     team_name, team_color = drivers_info[racer_name]
     racer = race.laps.pick_driver(racer_name)
     if team_name in teams:
-        plot_type = '+'
+        plot_type = "+"
     else:
         teams.append(team_name)
-    ax.plot(racer['LapNumber'], racer['LapTime'], color='#'+team_color, marker=plot_type, label=racer_name)
+    ax.plot(
+        racer["LapNumber"],
+        racer["LapTime"],
+        color="#" + team_color,
+        marker=plot_type,
+        label=racer_name,
+    )
 
 ax.set_title(" vs ".join(selected_racers))
 ax.set_xlabel("Lap Number")
@@ -64,5 +70,5 @@ ax.set_ylabel("Lap Time")
 ax.legend(loc="best")
 
 
-st.write('')
+st.write("")
 st.pyplot(fig)
